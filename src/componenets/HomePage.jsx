97bfa51afda32d969ec9f9b2fedd7f20f630efb1
Chris Langager <christopher.langager@gmail.com>
2020-12-12 14:36:11 -0700

    some basic styling

--- src/componenets/HomePage.jsx
+++ src/componenets/HomePage.jsx
@@ -4,25 +4,46 @@ import { Shampoo } from './Shampoo';
 export const HomePage = (props) => {
   const { shampoos } = props;
   return (
-    <div>
-      <Head>
-        <title>Shampoo Data</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <div>
+        <Head>
+          <title>Shampoo Data</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main>
-        <h1>Shampoo Data</h1>
+        <main>
+          <h1>Shampoo Data</h1>
+          <p>We have data about shampoo.</p>
 
-        <p>This sure is a home page</p>
+          <input className="search" type="text" value="search placeholder..." />
 
-        <ul>
-          {shampoos.map((shampoo) => (
-            <li>
-              <Shampoo key={shampoo.id} {...shampoo} />
-            </li>
-          ))}
-        </ul>
-      </main>
-    </div>
+          <ul className="shampoos">
+            {shampoos.map((shampoo) => (
+              <li>
+                <Shampoo key={shampoo.id} {...shampoo} />
+              </li>
+            ))}
+          </ul>
+        </main>
+      </div>
+
+      <style jsx>{`
+        .shampoos {
+          list-style-type: none;
+        }
+        ol,
+        ul {
+          margin: 0;
+          padding: 0;
+        }
+        li {
+          margin-bottom: 0.75rem;
+        }
+
+        .search {
+          margin: 1.5rem 0;
+        }
+      `}</style>
+    </>
   );
 };
--- src/componenets/Shampoo.jsx
+++ src/componenets/Shampoo.jsx
@@ -1,10 +1,31 @@
 export const Shampoo = (props) => {
   const { name, brand, ingredients } = props;
   return (
-    <div className="shampoo">
-      <h2>{name}</h2>
-      <h4>{brand.name}</h4>
-      <p>{ingredients.map(({ name }) => name).join(', ')}</p>
-    </div>
+    <>
+      <div className="shampoo">
+        <div className="icon">
+          <img src="./default.svg" alt="shampoo bottle" />
+        </div>
+        <div className="info">
+          <h2>{name}</h2>
+          <h4>{brand.name}</h4>
+          <p>{ingredients.map(({ name }) => name).join(', ')}</p>
+        </div>
+      </div>
+      <style jsx>{`
+        .shampoo {
+          display: grid;
+          grid-template-columns: 1fr 3fr;
+
+          border: 1px solid grey;
+          padding: 0.75rem;
+        }
+
+        .icon {
+          height: 100px;
+          width: 100px;
+        }
+      `}</style>
+    </>
   );
 };
